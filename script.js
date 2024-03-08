9a6a2fb10de0095118f1b5d4e00359fd6073a3ce
ankitpawar1 <54860507+ankitpawar1@users.noreply.github.com>
2024-03-08 23:19:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function fibonacci(num) {
 // your code here
+	if (num === 0) {
+        return 0;
+    } else if (num === 1) {
+        return 1;
+    } else {
+        return fibonacci(num - 1) + fibonacci(num - 2);
+    }
 }
+const result = fibonacci(5);
+const fibonacciResultElement = document.getElementById("fibonacciResult");
+fibonacciResultElement.textContent = result;
 
 module.exports = fibonacci;
