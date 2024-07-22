484cbdac0ce65f63d12a3d8cda6e5d1ec3aa24ef
ankitpawar1 <54860507+ankitpawar1@users.noreply.github.com>
2024-07-22 11:47:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,22 @@
 function fibonacci(num) {
-  if (num <= 0) {
-      return "Invalid input";
-  } else if (num === 1) {
-      return 0;
-  } else if (num === 2) {
-      return 1;
-  }
+    if (num === 0) return 0;
+    if (num === 1) return 1;
 
-  let a = 0;
-  let b = 1;
-  let temp;
-
-  for (let i = 2; i < num; i++) {
-      temp = a + b;
-      a = b;
-      b = temp;
-  }
-
-  return b;
+    let a = 0, b = 1;
+    for (let i = 2; i <= num; i++) {
+        let temp = a + b;
+        a = b;
+        b = temp;
+    }
+    return b;
 }
 
-console.log(fibonacci(1));
-console.log(fibonacci(5));
-console.log(fibonacci(10));
+function calculateFibonacci() {
+    const num = parseInt(document.getElementById('fibonacciInput').value);
+    if (isNaN(num) || num < 0 || num > 50) {
+        alert('Please enter a positive integer between 0 and 50.');
+        return;
+    }
+    const result = fibonacci(num);
+    document.getElementById('fibonacciResult').innerText = result;
+}
